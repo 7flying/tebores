2bfa79fa228943b2222e21592b9ae7e4adf49ff4
7flying <diez.f.irene@gmail.com>
2014-04-24 23:04:05 +0200

    db creation

--- tebores/manager/dbmanager.py
+++ tebores/manager/dbmanager.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-
+from os.path import isfile
 import sqlite3 as lite
 import sys
-
+"""
 conn = None
 
 try:
@@ -18,3 +18,64 @@ except lite.Error, e:
 finally:
 	if conn:
 		conn.close()
+
+"""
+class DBManager(object):
+	
+	conn = None
+	dbname = 'tebores_test.db'
+	
+	def __init__(self):
+		if not isfile(DBManager.dbname):
+			self.connect()
+			cursor = DBManager.conn.cursor()
+			cursor.execute(
+				"""CREATE TABLE Webs (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+				 name TEXT NOT NULL,
+				 url TEXT NOT NULL UNIQUE);"""
+			)
+			cursor.execute(
+				"""CREATE TABLE Books (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+				idWeb INTEGER NOT NULL REFERENCES Webs (id) ON DELETE CASCADE,
+				name TEXT NOT NULL,
+				url TEXT NOT NULL UNIQUE);"""
+			)
+
+			#cursor.execute("INSERT INTO Webs (name, url) VALUES ('it-ebooks', 'http://it-ebooks.info/')")
+			#cursor.execute("INSERT INTO Webs (name, url) VALUES ('freecomputerbooks', 'http://freecomputerbooks.com/')")
+
+			DBManager.conn.commit()
+			self.disconnect()
+		else:
+			print "Database already created"
+
+
+	def connect(self):
+		if DBManager.conn == None:
+			try:
+				DBManager.conn = lite.connect(DBManager.dbname)
+			except lite.Error, e:
+				print "Exception %s" % e.args[0]
+
+	def disconnect(self):
+		if DBManager.conn:
+			DBManager.conn.close()
+
+	def insert_book(self, book_web_url, book_name, book_url):
+		cursor = DBManager.conn.cursor()
+		# Get book_web_url id
+		cursor.execute("SELECT id FROM Webs WHERE url = ?;", (book_web_url,))
+		result = cursor.fetchone()
+		if result:
+			cursor.execute("INSERT INTO Books (idWeb, name, url) VALUES (?, ?, ?);", (result, book_name, book_url))
+			DBManager.conn.commit()
+			print "book inserted"
+		else:
+			print "not fetched"
+
+
+if __name__ == '__main__':
+	manager = DBManager()
+	manager.connect()
+	manager.insert_book("http://it-ebooks.info/", "some book name", "some book url")
+	manager.disconnect()
